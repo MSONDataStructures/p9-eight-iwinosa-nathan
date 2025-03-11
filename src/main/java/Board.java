e8e877e8a72247ff184488254094a30d329a9db6
NathanBreit <ngbreit@icloud.com>
2025-03-10 22:45:38 -0500

    Board Class Finished Except for the Neighbors method.

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,6 +1,7 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.MinPQ;
+import java.lang.StringBuilder;
 
 public class Board {
     private final int[][] blocks;
@@ -40,7 +41,23 @@ public class Board {
 
     public int manhattan() {
         // sum of Manhattan distances between blocks and goal
-        return 0;
+        int count = 0;
+        int rowOfGoal;
+        int colOfGoal;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int block = blocks[i][j];
+                if (block != 0) {
+                    rowOfGoal = (int) ((block - 1) / n);
+                    colOfGoal = (int) ((block - 1) % n);
+                    //praise intelliJ autofill for giving me this logic
+
+                    count += (Math.abs(i - rowOfGoal) + Math.abs(j - colOfGoal));
+                }
+
+            }
+        }
+        return count;
     }
 
     public String toString() {
@@ -50,17 +67,60 @@ public class Board {
         //  1 0 3
         //  4 2 5
         //  7 8 6
-        return null;
+
+        StringBuilder stringRepresentation = new StringBuilder();
+        //constructs string
+
+        stringRepresentation.append(n);
+        stringRepresentation.append("\n");
+        //sets up the size of the visualization
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                stringRepresentation.append(blocks[i][j] + " ");
+                //adds each element to string
+            }
+            stringRepresentation.append("\n");
+            //line break every row
+        }
+        return stringRepresentation.toString();
     }
 
     public boolean equals(Object y) {
         // does this board equal y?
-        return false;
+        if (this.n != ((Board) y).n) {
+            return false;
+        }
+        boolean returnBoolean = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (this.blocks[i][j] != ((Board) y).blocks[i][j]) {
+                    returnBoolean = false;
+                }
+            }
+        }
+        return returnBoolean;
     }
 
     public Board twin() {
         // a board that is obtained by exchanging any pair of blocks
-        return null;
+        int[][] twinBuilder = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                twinBuilder[i][j] = blocks[i][j];
+            }
+        }
+
+        if (twinBuilder[0][0] == 0 || twinBuilder[0][1] == 0) {
+            int temp = twinBuilder[1][0];
+            twinBuilder[1][0] = twinBuilder[1][1];
+            twinBuilder[1][1] = temp;
+        } else {
+            int temp = twinBuilder[0][0];
+            twinBuilder[0][0] = twinBuilder[0][1];
+            twinBuilder[0][1] = temp;
+        }
+        return new Board(twinBuilder);
     }
 
     public boolean isGoal() {
@@ -83,6 +143,30 @@ public class Board {
 
     public Iterable<Board> neighbors() {
         // all neighboring boards
+        int mainRow = 0;
+        int mainCol = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (blocks[i][j] == 0) {
+                    mainRow = j;
+                    mainCol = i;
+                }
+            }
+        }
+
+        int leftRow = mainRow - 1;
+        int leftCol = mainCol;
+        int rightRow = mainRow + 1;
+        int rightCol = mainCol;
+        int upRow = mainRow;
+        int upCol = mainCol - 1;
+        int downRow = mainRow;
+        int downCol = mainCol + 1;
+
+        if (leftRow >= 0 && leftRow < n && leftCol >= 0 && leftCol < n) {
+
+        }
+        //will finish later.
         return null;
     }
 
